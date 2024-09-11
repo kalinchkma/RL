4c6d32e2b2296231f38ef2395387a5ad0bc9551b
kalinchkma <chakma.kalin10211@gmail.com>
2024-09-11 23:16:35 +0600

    bandit class added for e silon-greedy

--- multi-armed bandit/greedy.py
+++ multi-armed bandit/greedy.py
@@ -1,4 +1,16 @@
 import matplotlib.pyplot as plt # type: ignore
 import numpy as np
 
+class Bandit:
+    def __init__(self, p) -> None:
+        self.p = p # win rate of bandit
+        self.p_estimate = 0.
+        self.N = 0. # number of sample collected so far
 
+    def pull(self):
+        # draw a 1 with probability p
+        return np.random.random() < self.p
+    
+    def update(self, x):
+        self.N += 1
+        self.p_estimate = ((self.N -1) * self.p_estimate + x)/self.N
